40673c2f8ce1ac60a25bae9f40f191c4900caaed
Khadija Nazir <bsai23006@itu.edu.pk>
2025-05-05 10:53:44 -0700

    used mui table

--- src/pages/Education.js
+++ src/pages/Education.js
@@ -1,42 +1,59 @@
 // src/pages/Education.js
 import React from "react";
+import {
+  TableContainer,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+  Paper,
+  Typography,
+} from "@mui/material";
 
 export default function Education() {
+  const rows = [
+    {
+      degree: "Bachelor of Science in Artificial Intelligence",
+      institute: "ITU Punjab",
+      location: "Lahore",
+      year: "2027",
+    },
+    {
+      degree: "Master of Science in Computer Science",
+      institute: "Future University",
+      location: "Lahore",
+      year: "2029",
+    },
+  ];
+
   return (
     <div className="right">
-      <div className="section-heading" id="education">
+      <Typography variant="h5" className="section-heading" id="education">
         Education
-      </div>
-      <div>
-        <table
-          id="education-table"
-          className="table table-striped"
-          style={{ maxWidth: "65vw", margin: "0 auto" }}
-        >
-          <thead>
-            <tr>
-              <th scope="col">Degree</th>
-              <th scope="col">Institute</th>
-              <th scope="col">Location</th>
-              <th scope="col">Year</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>Bachelor of Science in Artificial Intelligence</td>
-              <td>ITU Punjab</td>
-              <td>Lahore</td>
-              <td>2027</td>
-            </tr>
-            <tr>
-              <td>Master of Science in Computer Science</td>
-              <td>Future University</td>
-              <td>Lahore</td>
-              <td>2029</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+      </Typography>
+      <TableContainer component={Paper} className="education-table-container">
+        <Table className="table table-striped" aria-label="education table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Degree</TableCell>
+              <TableCell>Institute</TableCell>
+              <TableCell>Location</TableCell>
+              <TableCell>Year</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow key={row.degree}>
+                <TableCell>{row.degree}</TableCell>
+                <TableCell>{row.institute}</TableCell>
+                <TableCell>{row.location}</TableCell>
+                <TableCell>{row.year}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   );
 }
